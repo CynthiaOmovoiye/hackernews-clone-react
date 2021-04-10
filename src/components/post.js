7994cfccde7129335ca5dd4ba2cfff9b662199d8
Omovoiye Cynthia <omovoiye@gmail.com>
2021-04-10 23:11:39 +0100

    update

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import axios from "axios";
-import '../styles.scss';
+import "../styles.scss";
 import React, { useState, useEffect } from "react";
 
 const Alink = ({ url, title }) => (
@@ -18,9 +18,7 @@ const Post = () => {
     setIsLoading(true);
     fetchpost();
   }, []);
-  useEffect(() => {
-    
-  }, [post]);
+  useEffect(() => {}, [post]);
   const fetchpost = async () => {
     const response = await axios(
       `https://node-hnapi.herokuapp.com${location.pathname}`
@@ -35,78 +33,77 @@ const Post = () => {
       ) : (
         <React.Fragment>
           <div className="story card">
-      <div className="card-header">
-        <div className="story-title">
-          <h4 className="font-weight-bold">
-            <Alink url={post.url} title={post.title} />
-            <span className="domain ml-2">
-              <Alink url={`https://${post.domain}`} title={post.domain} />
-            </span>
-          </h4>
-        </div>
-        <div className="story-info d-flex flex-column flex-md-row align-items-md-center">
-          <div className="d-flex post-info">
-            <span className="w-50">
-              <i
-                className="fas fa-comments"
-                style={{
-                  fontSize: "10px",
-                  marginTop: "16px",
-                  padding: "0px",
-                  marginRight: "2px",
-                  color: "#d3d3d3",
-                }}
-              />
+            <div className="card-header">
+              <div className="story-title">
+                <h4 className="font-weight-bold">
+                  <Alink url={post.url} title={post.title} />
+                  <span className="domain ml-2">
+                    <Alink url={`https://${post.domain}`} title={post.domain} />
+                  </span>
+                </h4>
+              </div>
+              <div className="story-info d-flex flex-column flex-md-row align-items-md-center">
+                <div className="d-flex post-info">
+                  <span className="w-50">
+                    <i
+                      className="fas fa-comments"
+                      style={{
+                        fontSize: "10px",
+                        marginTop: "16px",
+                        padding: "0px",
+                        marginRight: "2px",
+                        color: "#d3d3d3",
+                      }}
+                    />
 
-              <span>{`${post.comments_count}`} Comments</span>
-            </span>
-            <span className="w-50">
-              <i
-                className="fas fa-clock"
-                style={{
-                  fontSize: "10px",
-                  marginTop: "16px",
-                  padding: "0px",
-                  marginRight: "2px",
-                  color: "#d3d3d3",
-                }}
-              />
-              {`${post.time_ago}`}
-            </span>
-          </div>
-          <div>
-            <span className="user">
-              Submitted by{" "}
-              <Alink
-                url={`https://news.ycombinator.com/user?id=${post.user}`}
-                title={post.user}
-              />
-            </span>
-          </div>
-        </div>
-      </div>
+                    <span>{`${post.comments_count}`} Comments</span>
+                  </span>
+                  <span className="w-50">
+                    <i
+                      className="fas fa-clock"
+                      style={{
+                        fontSize: "10px",
+                        marginTop: "16px",
+                        padding: "0px",
+                        marginRight: "2px",
+                        color: "#d3d3d3",
+                      }}
+                    />
+                    {`${post.time_ago}`}
+                  </span>
+                </div>
+                <div>
+                  <span className="user">
+                    Submitted by{" "}
+                    <Alink
+                      url={`https://news.ycombinator.com/user?id=${post.user}`}
+                      title={post.user}
+                    />
+                  </span>
+                </div>
+              </div>
+            </div>
 
-      <div className="card-body">
-        <h4>Comments</h4>
-        <ul>
-          {post.comments &&
-            post.comments.map((Parentcomment) => (
-              <Comments
-                key={Parentcomment.id}
-                comment={Parentcomment}
-                type="parent"
-              />
-            ))}
-        </ul>
-      </div>
-    </div>
+            <div className="card-body">
+              <h4>Comments</h4>
+              <ul>
+                {post.comments &&
+                  post.comments.map((Parentcomment) => (
+                    <Comments
+                      key={Parentcomment.id}
+                      comment={Parentcomment}
+                      type="parent"
+                    />
+                  ))}
+              </ul>
+            </div>
+          </div>
         </React.Fragment>
       )}
     </React.Fragment>
-   
   );
 };
-function Comments({ comment}) {
+function Comments({ comment }) {
   const [showChildren, setShowChildren] = useState(true);
 
   // this causes the data to check if there are more "children" comments under
@@ -116,11 +113,7 @@ function Comments({ comment}) {
     return (
       <ul>
         {" "}
-        <Comments
-          key={Childcomment.id}
-          comment={Childcomment}
-          type="child"
-        />
+        <Comments key={Childcomment.id} comment={Childcomment} type="child" />
       </ul>
     );
   });
@@ -132,25 +125,23 @@ function Comments({ comment}) {
     <li>
       <div
         style={{ display: "inline", fontWeight: "bold", fontSize: "large" }}
-        onClick={() => setShowChildren(!showChildren)}>
-          {showChildren ? (
-      <i className="fas fa-caret-down comment-icons" ></i>
-    ) : (
-      <i className="fas fa-caret-up comment-icons"
-     
-     
-      ></i>
-    )}
-        
-        
+        onClick={() => setShowChildren(!showChildren)}
+      >
+        {showChildren ? (
+          <i className="fas fa-caret-down comment-icons"></i>
+        ) : (
+          <i className="fas fa-caret-up comment-icons"></i>
+        )}
+
         <span className="comment-user">
           <Alink
             url={`https://news.ycombinator.com/user?id=${comment.user}`}
             title={comment.user}
           />
         </span>
-        <span style={{ fontSize: "10pt", color: "#9d9ca0" }}>{comment.time_ago}</span>
-        
+        <span style={{ fontSize: "10pt", color: "#9d9ca0" }}>
+          {comment.time_ago}
+        </span>
       </div>
       {/* this left border is the line that connects the comments on the same level in the thread */}
       {showChildren && (
@@ -189,5 +180,3 @@ function Comments({ comment}) {
 }
 
 export default Post;
-
-
